77fb9a7f2ad6071dbcaeca76e0fc54895b3b0bc1
AldarUlyumdzhiev <aldarulyumdzhiev@yandex.ru>
2025-07-13 17:40:14 +0300

    add spring boot project

---
+++ backend/src/test/java/com/sportlink/backend/SportLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportlink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
