0a0dc139a60493f6ba71774710ea838bfe18d5c2
AldarUlyumdzhiev <aldarulyumdzhiev@yandex.ru>
2025-10-01 03:16:10 +0300

    структура модульный монолит

---
+++ backend/app/src/main/java/com/sportlink/sport/SportLinkApplication.java
@@ -0,0 +1,13 @@
+package com.sportlink.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportLinkApplication.class, args);
+	}
+
+}
---
+++ backend/modules/common/src/test/java/com/sportlink/admin/SportLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportlink.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
